78f9fe5df64f4c93a211893c31412703b640f228
sarasaurus <bixler.s.e@gmail.com>
2018-04-19 17:54:43 -0700

    bmp file logging to console

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
+const fs = require('fs');
+
+//const parseBitmap = require('./lib/parse-bitmap');
+
+//import { fstat } from "fs";
+
+
 console.log('SUP');
+
+fs.readFile (`${__dirname}/__test__/asset/test.bmp`, (error, data)=> {
+    if (error) {
+        throw error;
+    } else {
+       console.log(data);
+    }
+})

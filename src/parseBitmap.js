b1c89507017d51001745a969abf6f86c2012419c
melody2m <melody2musik@gmail.com>
2018-04-20 16:42:03 -0700

    using parseBitmap

--- src/parseBitmap.js
+++ src/parseBitmap.js
@@ -1,19 +1,22 @@
 'use strict';
 
-parseBitmap.parse = (buffer) => {
-    if (error) {
-        throw error;
-    }
-    const parsedBitmap = {};
-    const FILE_SIZE_OFFSET = 2;
-    const HEIGHT_OFFSET = 2;
-    const COLOR_TABLE_OFFSET = 54;
-    const COLOR_TABLB_SIZE = 4096;
+const parseBitmap = module.exports = {};
 
-    parsedBitmap.type = buffer.toString('utf-8', 0, 2);
-    parsedBitmap.fileSize = buffer.readInt32LE(FILE_SIZE_OFFSET);
-    parsedBitmap.height = buffer.readInt32LE(HEIGHT_OFFSET);
-    parsedBitmap.colorTable = buffer.slice(COLOR_TABLE_OFFSET, COLOR_TABLB_SIZE);
+parseBitmap.parse = (error, buffer) => {
+  if (error) {
+    throw error;
+  }
+  const parsedBitmap = {};
+  const FILE_SIZE_OFFSET = 2;
+  const HEIGHT_OFFSET = 22;
+  const COLOR_TABLE_OFFSET = 54;
+  const COLOR_TABLE_SIZE = 1024;
 
-    return parsedBitmap;
+  parsedBitmap.type = buffer.toString('utf-8', 0, 2);
+  parsedBitmap.fileSize = buffer.readInt32LE(FILE_SIZE_OFFSET);
+  parsedBitmap.height = buffer.readInt32LE(HEIGHT_OFFSET);
+  parsedBitmap.colorTable = buffer.slice(COLOR_TABLE_OFFSET, COLOR_TABLE_SIZE);
+
+  console.log(parsedBitmap);
+  return parsedBitmap;
 };

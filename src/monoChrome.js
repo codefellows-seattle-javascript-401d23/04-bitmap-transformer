96ee483b5b0e3e6eab68b01eb4694826a6e82989
melody2m <melody2musik@gmail.com>
2018-04-21 15:37:40 -0700

    updates image

--- src/monoChrome.js
+++ src/monoChrome.js
@@ -1,11 +1,17 @@
 
 const monoChrome = module.exports = {};
 
-monoChrome.transform = (writePath, buffer, callback) => {
+monoChrome.transform = (buffer, callback) => {
   const COLOR_TABLE_OFFSET = 54;
   const COLOR_TABLE_SIZE = 1024;
 
-  let colorTable = buffer.slice(COLOR_TABLE_OFFSET, COLOR_TABLE_SIZE);  
+  const colorTable = buffer.slice(COLOR_TABLE_OFFSET, COLOR_TABLE_SIZE); 
+
+  for (let i = 0; i < colorTable.length; i++) {
+    colorTable[i] = 0;
+  }
+
   console.log(colorTable);
-  callback(null, buffer);
+  
+  callback(buffer);
 };

6a89e4cb83e224acc7d0469a9ba3142cabeb4fe0
Jennifer Williams <jennifer@squareandplum.com>
2018-04-21 14:37:08 -0700

    add ability to take in transform name from console

--- src/main.js
+++ src/main.js
@@ -1,33 +1,55 @@
-'use strict'
+'use strict';
 
 const fs = require('fs');
+const logger = require('./lib/logger');
+const readline = require('readline');
 const parseBitmap = require('./lib/parse-bitmap');
 const invertColors = require('./lib/invert-colors');
-// const lightenColors = require('./lib/lighten-colors');
+const lightenColors = require('./lib/lighten-colors');
 
 // todo: take in <input file name>, <output file name>, <transform> from user
 
+
 const inputPath = `${__dirname}/assets/house.bmp`;
 const outputPath = `${__dirname}/assets/newBitmap.bmp`;
-// const transform = 'lighten';
-const transform = 'invert';
+let transform = '';
 
-fs.readFile(inputPath, (error, buffer) => {
-  if (error) {
-    throw error;
-  }
-  console.log(parseBitmap.parse(buffer));
-  
-  if (transform === 'invert') {
-    invertColors.invert(buffer, (invertedBuffer) => {
-      fs.writeFile(outputPath, invertedBuffer, (err) => {
-        if (err) {
-          throw err;
-        }
-        console.log('buffer written to newBitmap.bmp');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Please enter a transform name. Choices: "invert" or "lighten".\n', (transformName) => {
+  console.log('processing...');
+  transform = transformName;
+  rl.close();
+  fs.readFile(inputPath, (error, buffer) => {
+    if (error) {
+      throw error;
+    }
+    // console.log(parseBitmap.parse(buffer));
+
+    if (transform === 'invert') {
+      invertColors.invert(buffer, (invertedBuffer) => {
+        fs.writeFile(outputPath, invertedBuffer, (err) => {
+          if (err) {
+            throw err;
+          }
+          logger.log(logger.INFO, `Transformed bitmap written to ${outputPath}`);
+        });
       });
-    });
-  }
-
-  console.log(parseBitmap.parse(buffer));
+    } else if (transform === 'lighten') {
+      lightenColors.lighten(buffer, (invertedBuffer) => {
+        fs.writeFile(outputPath, invertedBuffer, (err) => {
+          if (err) {
+            throw err;
+          }
+          logger.log(logger.INFO, `Transformed bitmap written to ${outputPath}`);
+        });
+      });
+      // console.log(parseBitmap.parse(buffer));
+    } else {
+      // todo: error handling
+    }
+  });
 });

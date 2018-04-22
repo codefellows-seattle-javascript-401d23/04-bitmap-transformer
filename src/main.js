629dfc4bcac177d8859a58e3af53fbf2ccd19848
danielvportlandia <danielshelton90@gmail.com>
2018-04-22 10:19:23 -0700

    able to create files.

--- src/main.js
+++ src/main.js
@@ -1,25 +1,39 @@
 'use strict';
 
+const fs = require('fs');
 const fileReader = require('./lib/reader');
 const parseBitmap = require('./lib/parse-bitmap');
 const midnight = require('./lib/transform/midnight');
+const whiteout = require('./lib/transform/whiteout');
+const redden = require('./lib/transform/redden');
 
 const bitmapPath = `${__dirname}/assets/house.bmp`;
 
-const printBitmap = (file) => {
-  console.log(file);
-};
-
-const readDataAsync = (path) => {
+const imageConverter = (path) => {
   fileReader.readAsync(path, (buffer) => {
-    printBitmap(path);
-    console.log(buffer);
-    const bitmapObj = parseBitmap.parse(buffer);
+    parseBitmap.parse(buffer, (bitmapObj) => {
+      console.log(bitmapObj);
+        console.log('raster size', bitmapObj.rasterDataString.length);
+      whiteout.convertImage(bitmapObj, (newObj) => {
+        fs.writeFileSync(`${__dirname}/assets/whiteout.bmp`, newObj.data);
+        // console.log('new', bitmapObj.colorTable);
+      });
+      redden.convertImage(bitmapObj, (newBuffer) => {
+        console.log('buffer2', buffer0);
+        // console.log(('new length', newBuffer.length));
+        // console.log('header length', )
+        // console.log('lengths 3', bitmapObj.colorTable.length);
+        fs.writeFileSync(`${__dirname}/assets/redden.bmp`, newBuffer);
+        // console.log('newBuffer', newBuffer.length);
 
+      });
+    });
   });
 };
 
-readDataAsync(bitmapPath);
+imageConverter(bitmapPath);
 
-//fs.writeFileSync for writing
-//get parsed bitmap to transform
+console.log('done');
+
+// fs.writeFileSync for writing
+// get parsed bitmap to transform

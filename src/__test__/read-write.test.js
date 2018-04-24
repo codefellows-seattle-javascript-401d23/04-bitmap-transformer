c0b1fb8f10f23434195075265f58cb29d4453268
Sarah Geyer <sjgeyer@comcast.net>
2018-04-23 18:05:10 -0700

    all tests actually working now

--- src/__test__/read-write.test.js
+++ src/__test__/read-write.test.js
@@ -8,34 +8,44 @@ const mockArray = [];
 const compareToArray = [];
 const houseImage = `${__dirname}/../assets/house.bmp`;
 
-beforeAll((done) => {
-  return fs.readFile(houseImage, (err, buffer) => {
-    mockArray.push(buffer);
-    done();
-  });
-});
+// beforeAll((done) => {
+//   return fs.readFile(houseImage, (err, buffer) => {
+//     mockArray.push(buffer);
+//     done();
+//   });
+// });
 
 describe('this is testing our file reader', () => {
-  test('should display our image from the file system', () => {
-    return readWrite.read(houseImage, (data) => {
-      compareToArray.push(data);
-      if (mockArray.length === compareToArray.length) {
-        expect(mockArray).toEqual(compareToArray);
-      }
+  test('should display our image from the file system', (done) => {
+    return fs.readFile(houseImage, (err, buffer) => {
+      mockArray.push(buffer);
+      return readWrite.read(houseImage, (error, data) => {
+        compareToArray.push(data);
+        if (mockArray.length === compareToArray.length) {
+          expect(mockArray).toEqual(compareToArray);
+        }
+        done();
+      });
     });
   });
-  test('should err out for non-existent first file', () => {
-    return readWrite.read(mockImage, (err) => {
+  test('should err out for non-existent first file', (done) => {
+    return readWrite.read(mockImage, (err, data) => {
       expect(err).toHaveProperty('errno');
+      expect(data).toEqual(undefined);
+      done();
     });
   });
-});
 
-describe('this is testing our file writer', () => {
-  test('should create a new file successfully!', () => {
-    const newFilePath = `${__dirname}/../assets/test.bmp`;
-    return readWrite.write(newFilePath, mockArray[0], () => {
-      expect(fs.existsSync(newFilePath)).toEqual(true);
+  describe('this is testing our file writer', () => {
+    test('should create a new file successfully!', (done) => {
+      return fs.readFile(houseImage, (err, buffer) => {
+        mockArray.push(buffer);
+        const newFilePath = `${__dirname}/../assets/test.bmp`;
+        return readWrite.write(mockArray[0], newFilePath, () => {
+          expect(fs.existsSync(newFilePath)).toEqual(true);
+          done();
+        });
+      });
     });
   });
 });
--- src/lib/read-write.js
+++ src/lib/read-write.js
@@ -8,18 +8,20 @@ fileHandler.read = (filePath, callback) => {
   return fs.readFile(filePath, (error, buffer) => {
     if (error) {
       logger.log(logger.ERROR, `fileHandler.read: ${error}`);
-      return error;
+      return callback(error);
     }
     const type = process.argv[4];
     return callback(null, buffer, type);
   });
 };
 
-fileHandler.write = (buffer, newFilePath) => {
+fileHandler.write = (buffer, newFilePath, callback) => {
   return fs.writeFile(newFilePath, buffer, (error) => {
     if (error) {
       logger.log(logger.ERROR, `fileHandler.write: ${error}`);
+      return callback(error);
     }
     logger.log(logger.VERBOSE, `File created at ${newFilePath}`);
+    return callback();
   });
 };

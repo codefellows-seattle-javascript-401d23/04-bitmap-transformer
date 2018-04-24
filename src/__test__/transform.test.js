5d2d132fa3c6936a0f74e525778477960b6bea43
LACY HOGAN <lacy.hogan@gmail.com>
2018-04-23 18:36:03 -0700

    Attempting tests

--- src/__test__/transform.test.js
+++ src/__test__/transform.test.js
@@ -1,24 +1,41 @@
 'use strict';
 
-const white = require('../lib/transform-white');
-const invert = require('../lib/transform-invert');
-const toxic = require('../lib/transform-toxic');
-const random = require('../lib/transform-random');
+// const white = require('../lib/transform-white');
+// const invert = require('../lib/transform-invert');
+// const toxic = require('../lib/transform-toxic');
+// const random = require('../lib/transform-random');
+// const parseBitmap = require('../lib/parse-bitmap');
+const fs = require('fs');
 
-describe('transform-white.test', () => {
-  test('Should tranform image to all white' () => {
-    expect()
-  })
-});
+// require('jest');
+const testBmp = fs.readFile('./asset/house.bmp', (err, data) => data);
+const invertBmp = fs.readFile('./transformations/invert-transform', (err, data) => data);
+const randomBmp = fs.readFile('./transformations/random-transform', (err, data) => data);
+const toxicBmp = fs.readFile('./transformations/toxic-transform', (err, data) => data);
+const whiteBmp = fs.readFile('./transformations/white-transform', (err, data) => data);
 
-describe('transform-invert.test', () => {
-  test()
-});
+describe('Transformation Modules', () => {
+  describe('transform-invert.test', () => {
+    it('Should make should the output is not the same as the input', () => {
+      expect(testBmp).not.toEqual(invertBmp);
+    });
+  });
 
-describe('transform-toxic.test', () => {
-  test()
-});
+  describe('transform-random.test', () => {
+    it('Should make should the output is not the same as the input', () => {
+      expect(testBmp).not.toEqual(randomBmp);
+    });
+  });
 
-describe('transform-random.test', () => {
-  test()
+  describe('transform-toxic.test', () => {
+    it('Should make should the output is not the same as the input', () => {
+      expect(testBmp).not.toEqual(toxicBmp);
+    });
+  });
+
+  describe('transform-white.test', () => {
+    it('Should make should the output is not the same as the input', () => {
+      expect(testBmp).not.toEqual(whiteBmp);
+    });
+  });
 });

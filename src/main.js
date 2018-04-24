f19e7b235afd49b0bd4b913b7efcad133f2c72ef
Joanna Coll <johannam999@gmail.com>
2018-04-23 18:22:38 -0700

    finished transformations

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+process.argv;
+
 const fs = require('fs');
 const fileReader = require('./lib/reader');
 const parseBitmap = require('./lib/parse-bitmap');
@@ -10,17 +12,26 @@ const nightVision = require('./lib/transform/night-vision');
 
 const bitmapPath = `${__dirname}/assets/house.bmp`;
 
+
 const imageConverter = (path) => {
   fileReader.readAsync(path, (buffer) => {
     parseBitmap.parse(buffer, (bitmapObj) => {
-      // const magentaBitmap = magenta.convertImage(bitmapObj);
-      // fs.writeFileSync(`${__dirname}/assets/magenta.bmp`, magentaBitmap);
-      // const yellowfyBitmap = yellowfy.convertImage(bitmapObj);
-      // fs.writeFileSync(`${__dirname}/assets/yellowfy.bmp`, yellowfyBitmap);
-      const midnightBitmap = midnight.convertImage(bitmapObj);
-      fs.writeFileSync(`${__dirname}/assets/midnight.bmp`, midnightBitmap);
-      // const nightVisionBitmap = nightVision.convertImage(bitmapObj);
-      // fs.writeFileSync(`${__dirname}/assets/night-vision.bmp`, nightVisionBitmap);
+      if (process.argv[3] === 'magenta') {
+        const magentaBitmap = magenta.convertImage(bitmapObj);
+        fs.writeFileSync(`${__dirname}/assets/magenta.bmp`, magentaBitmap);
+      }
+      if (process.argv[3] === 'yellowfy') {
+        const yellowfyBitmap = yellowfy.convertImage(bitmapObj);
+        fs.writeFileSync(`${__dirname}/assets/yellowfy.bmp`, yellowfyBitmap);
+      }
+      if (process.argv[3] === 'midnight') {
+        const midnightBitmap = midnight.convertImage(bitmapObj);
+        fs.writeFileSync(`${__dirname}/assets/midnight.bmp`, midnightBitmap);
+      }
+      if (process.argv[3] === 'nightVision') {
+        const nightVisionBitmap = nightVision.convertImage(bitmapObj);
+        fs.writeFileSync(`${__dirname}/assets/night-vision.bmp`, nightVisionBitmap);
+      }
     });
   });
 };
